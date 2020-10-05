ded12977358d8931b7595066584cc312f7176f8f
elenamlopez <elena.mateos1@gmail.com>
2020-10-05 17:29:35 +0200

    Initial Commit

---
+++ FizzBuzz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FizzBuzz
+//
+//  Created by Elena on 05/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Elena on 05/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
