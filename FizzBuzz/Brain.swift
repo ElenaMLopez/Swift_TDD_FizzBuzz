20b70b8a202c9ed915e91811e4c22cb9bb2055ec
elenamlopez <elena.mateos1@gmail.com>
2020-10-05 18:54:32 +0200

    Brain class refactor

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -9,26 +9,22 @@ import Foundation
 
 class Brain {
     
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+    }
+    
     
     func isDivisibleByThree(number: Int) -> Bool {
-        if number % 3 == 0 {
-            return true
-        } else {
-            return false
-        }
+        return isDivisibleBy(divisor: 3, number: number)
     }
     func isDivisibleByFive(number: Int) -> Bool {
-        if number % 5 == 0 {
-            return true
-        } else {
-            return false
-        }
+        return isDivisibleBy(divisor: 5, number: number)
     }
     func isDivisibleByFifteen(number: Int) -> Bool {
-        if number % 15 == 0 {
-            return true
-        } else {
-            return false
-        }
+        return isDivisibleBy(divisor: 15, number: number)
     }
+    
+
+    
+    
 }
